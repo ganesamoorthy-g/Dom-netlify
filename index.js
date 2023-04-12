d7db85c1b89cd8136007f2a79a4edb2be1cbf374
ganesamoorthy-g <127511295+ganesamoorthy-g@users.noreply.github.com>
2023-04-12 19:04:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function foo(){
+    let res=document.getElementById("Firstname").value;
+    let res1=document.getElementById("Middlename").value;
+    let res2=document.getElementById("lastname").value;
+    let res3=document.getElementById("email").value;
+    let res4=document.getElementById("address").value;
+
+    console.log(res)
+    console.log(res1)
+    console.log(res2)
+    console.log(res3)
+    console.log(res4)
+
+   
+}
