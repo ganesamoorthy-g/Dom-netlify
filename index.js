55dffe70a78e30e80ebd9d140d10478b5fe8a9e6
ganesamoorthy-g <127511295+ganesamoorthy-g@users.noreply.github.com>
2023-04-12 18:25:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function foo(){
+    var firstname=document.getElementById("fname").value;
+    console.log(`Firstname:${firstname}`);
+    
+    var middlename=document.getElementById("mname").value;
+    console.log(`Middlename:${middlename}`);
+     
+    let res=document.getElementById("mail").value;
+    console.log(res);
+}
